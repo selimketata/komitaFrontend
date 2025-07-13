625df7d4ee21d91307079555e3df383cc57662bd
selimketata <ketata.15.selim@gmail.com>
2025-07-13 19:46:35 +0100

    add content.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,27 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-export default defineConfig(() => {
-  return {
-    assetsInclude: "**/*.JPG",
-    build: {
-      outDir: 'build',
+export default defineConfig(() => ({
+  assetsInclude: "**/*.JPG",
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@config': path.resolve(__dirname, 'src/config'),
     },
-    plugins: [react()],
-    server: {
-      host: '0.0.0.0',
-      port: process.env.PORT || 4200,
-      proxy: {
-        '/api': {
-          target: 'https://komita-backend.onrender.com',
-          changeOrigin: true,
-          secure: false,
-        },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4200,
+    proxy: {
+      '/api': {
+        target: 'https://komita-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
       },
     },
-    resolve: {
-      alias: {
-        '@config': path.resolve(__dirname, 'src/config'),
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
-        '@services': path.resolve(__dirname, 'src/Services'),
-        '@context': path.resolve(__dirname, 'src/Context'),
-      },
-    },
-  };
-});
+  },
+}));

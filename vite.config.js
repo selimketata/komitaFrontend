190c9b023ac48471563479e1cbe42e8550852984
selimketata <ketata.15.selim@gmail.com>
2025-07-13 19:40:43 +0100

    add content.js

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig(() => {
     },
     plugins: [react()],
     server: {
-      host: '0.0.0.0', // Allows external access
-      port: process.env.PORT || 4200, // Uses dynamic port or defaults to 4200
+      host: '0.0.0.0',
+      port: process.env.PORT || 4200,
       proxy: {
         '/api': {
           target: 'https://komita-backend.onrender.com',
@@ -22,12 +22,11 @@ export default defineConfig(() => {
     },
     resolve: {
       alias: {
-        '@config': path.resolve(__dirname, './src/Config'),
-        '@components': path.resolve(__dirname, './src/components'),
-        '@pages': path.resolve(__dirname, './src/pages'),
-        '@services': path.resolve(__dirname, './src/Services'),
-        '@context': path.resolve(__dirname, './src/Context'),
-        // Ajoute plus d'alias si nécessaire
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@services': path.resolve(__dirname, 'src/Services'),
+        '@context': path.resolve(__dirname, 'src/Context'),
       },
     },
   };

7712d6252f47825e0be749a34959dce09254db1f
selimketata <ketata.15.selim@gmail.com>
2025-07-19 22:26:18 +0100

    Fix issues 3

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
-      '@config': path.resolve(__dirname, 'src/config'),
+      '@config': path.resolve(__dirname, 'config'),
     },
   },
   server: {

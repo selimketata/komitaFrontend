a11b63ba4d797a1b82abaf55ab7677095140af78
selimketata <ketata.15.selim@gmail.com>
2025-07-19 22:16:48 +0100

    Fix issues

--- src/Config/content/aboutContent.js
+++ src/Config/content/aboutContent.js
@@ -19,7 +19,7 @@ import bac from "/backg komita.png";
 import flag from "/Capture 1.png";
 import logo from "/komita.png";
 
-export const aboutContent = {
+const aboutContent = {
   // Images and icons object to export all image references
   images: {
     man,
